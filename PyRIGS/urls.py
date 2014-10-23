33b454684d0410755417073e266ce9185cee2dac
tjp03u <psytp@nottingham.ac.uk>
2014-10-23 23:42:53 +0100

    Set up static files correctly
    Working authentication

--- PyRIGS/urls.py
+++ PyRIGS/urls.py
@@ -1,14 +1,20 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+import RIGS
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'PyRIGS.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^user/login$', 'RIGS.views.login', name='login'),
+    url(r'^', include('RIGS.urls')),
     url('^user/', include('django.contrib.auth.urls')),
     url('^user/', include('registration.backends.default.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
