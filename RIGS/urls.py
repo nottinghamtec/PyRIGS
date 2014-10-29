4210c192808aa21c98e8ac663bec4c2295f5559d
tomtom5152 <tom@codedinternet.com>
2014-10-29 14:39:10 +0000

    Implemented person listing

--- RIGS/urls.py
+++ RIGS/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.decorators import login_required
 from RIGS import views
+
+from PyRIGS.decorators import permission_required_with_403
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,6 +12,6 @@ urlpatterns = patterns('',
     url('^user/login/$', 'RIGS.views.login', name='login'),
 
     # People
-    url(r'^people/$', views.PersonIndex.as_view(), name='person')
+    url(r'^people/$', permission_required_with_403('RIGS.view_person')(views.PersonIndex.as_view()), name='person')
 )
 
