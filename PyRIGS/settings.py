d8f399bfe3bbf9edc6a4195cd0c7ce4e0b1480f9
Tom Price <tom@codedinternet.com>
2014-11-04 00:11:04 +0000

    Added buttons to view and edit people from index.
    Updated additional ajax realted template things

--- PyRIGS/settings.py
+++ PyRIGS/settings.py
@@ -99,6 +99,17 @@ USE_L10N = True
 
 USE_TZ = True
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.core.context_processors.request",
+    "django.contrib.messages.context_processors.messages",
+)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
