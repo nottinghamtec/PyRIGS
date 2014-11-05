e2a60a5fde20468098a40a5b9b5e2ceac811928f
tomtom5152 <tom@codedinternet.com>
2014-11-05 19:34:36 +0000

    Added Venue, Event and EventItem models

--- RIGS/admin.py
+++ RIGS/admin.py
@@ -6,3 +6,6 @@ import reversion
 admin.site.register(models.Person, reversion.VersionAdmin)
 admin.site.register(models.Organisation, reversion.VersionAdmin)
 admin.site.register(models.VatRate, reversion.VersionAdmin)
+admin.site.register(models.Venue, reversion.VersionAdmin)
+admin.site.register(models.Event, reversion.VersionAdmin)
+admin.site.register(models.EventItem, reversion.VersionAdmin)
