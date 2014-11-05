5921c01ef189e1db2612b1cb1e0cfffb5bfb7d8a
tomtom5152 <tom@codedinternet.com>
2014-11-05 15:57:34 +0000

    Updated organisation templates.
    Render improvements to forms
    
    Fixed organisation url

---
+++ RIGS/migrations/0006_auto_20141105_1553.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('RIGS', '0005_auto_20141104_1619'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='organisation',
+            old_name='unionAccount',
+            new_name='union_account',
+        ),
+    ]
