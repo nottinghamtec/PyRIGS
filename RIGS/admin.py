340db92a1fc7c2ceb20db1ee1438da9b07f9521e
tomtom5152 <tom@codedinternet.com>
2014-11-05 16:50:55 +0000

    Added Person, Organisation and VatRate to admin section with reversion support

--- RIGS/admin.py
+++ RIGS/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from RIGS import models
+import reversion
 
 # Register your models here.
+admin.site.register(models.Person, reversion.VersionAdmin)
+admin.site.register(models.Organisation, reversion.VersionAdmin)
+admin.site.register(models.VatRate, reversion.VersionAdmin)
