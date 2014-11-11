c832bb924afe53a79dac69ec02b22a5a66fb7a39
tec <tec@alfie.codedinternet.com>
2014-11-11 20:08:36 +0000

    Added all event views and urls. Added event detail template.
    Added itemisation templates.

--- RIGS/rigboard.py
+++ RIGS/rigboard.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.core.urlresolvers import reverse_lazy
 from RIGS import models
 
 __author__ = 'ghost'
@@ -14,3 +15,28 @@ class RigboardIndex(generic.TemplateView):
         # call out method to get current events
         context['events'] = models.Event.objects.current_events()
         return context
+
+class EventDetail(generic.DetailView):
+    model = models.Event
+
+class EventCreate(generic.CreateView):
+    model = models.Event
+
+    def get_context_data(self, **kwargs):
+    	context = super(EventCreate, self).get_context_data(**kwargs)
+	context['edit'] = True
+	return context
+    
+    def get_success_url(self):
+        return reverse_laze('event_detail', kwargs={'pk': self.object.pk})
+
+class EventUpdate(generic.UpdateView):
+    model = models.Event
+
+    def get_context_data(self, **kwargs):
+    	context = super(EventUpdate, self).get_context_data(**kwargs)
+	context['edit'] = True
+	return context
+
+    def get_success_url(self):
+        return reverse_laze('event_detail', kwargs={'pk': self.object.pk})
