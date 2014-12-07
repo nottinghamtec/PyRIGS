bb297aebb1e05eb91013abb97561bc086c3f4170
Tom Price <tom@codedinternet.com>
2014-12-07 19:46:46 +0000

    Migrated reportlab and rml to requirements.txt following working compiler

--- z3c/rml/__init__.py
+++
@@ -1,7 +0,0 @@
-# Hook up our custom paragraph parser.
-import z3c.rml.paraparser
-import z3c.rml.rlfix
-
-
-from reportlab.lib.styles import getSampleStyleSheet
-SampleStyleSheet = getSampleStyleSheet()
