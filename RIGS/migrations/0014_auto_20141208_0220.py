b52da8c2c0303fb492fe08189e3c9c7d6f9ea477
Tom Price <tom@codedinternet.com>
2014-12-08 02:42:18 +0000

    Changes to importer: Added delete method to all importers Added nonrig importing Venue import optimisation
    Reran importer for empty DB
    Reset password appropriately
    
    Added view permission to all models where needed.
    Migrated DB to reflect.
    
    Fixed all print buttons on event detail.

---
+++ RIGS/migrations/0014_auto_20141208_0220.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('RIGS', '0013_auto_20141202_0041'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='event',
+            name='venue',
+            field=models.ForeignKey(blank=True, to='RIGS.Venue', null=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='eventitem',
+            name='event',
+            field=models.ForeignKey(related_name='items', blank=True, to='RIGS.Event'),
+            preserve_default=True,
+        ),
+    ]
---
+++ RIGS/migrations/0015_auto_20141208_0233.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('RIGS', '0014_auto_20141208_0220'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='event',
+            options={'permissions': (('view_event', 'Can view Events'),)},
+        ),
+        migrations.AlterModelOptions(
+            name='organisation',
+            options={'permissions': (('view_organisation', 'Can view Organisations'),)},
+        ),
+        migrations.AlterModelOptions(
+            name='person',
+            options={'permissions': (('view_person', 'Can view Persons'),)},
+        ),
+        migrations.AlterModelOptions(
+            name='venue',
+            options={'permissions': (('view_venue', 'Can view Venues'),)},
+        ),
+    ]
