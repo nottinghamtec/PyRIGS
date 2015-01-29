2ce45b9297fd0e476d44d90dc201b8d9a22b11c4
tomtom5152 <tom@codedinternet.com>
2015-01-29 23:17:50 +0000

    Basic working invoice system. Need to add a way to create invoices.

---
+++ RIGS/migrations/0017_auto_20150129_2041.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('RIGS', '0016_auto_20150127_1905'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='invoice',
+            options={'permissions': (('view_invoice', 'Can view Invoices'),)},
+        ),
+    ]
