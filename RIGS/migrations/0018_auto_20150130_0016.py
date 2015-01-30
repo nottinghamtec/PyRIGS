af1396a1686d30616673ff8f35135111e61aed5f
tomtom5152 <tom@codedinternet.com>
2015-01-30 00:26:15 +0000

    Added the ability to invoice an event

---
+++ RIGS/migrations/0018_auto_20150130_0016.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('RIGS', '0017_auto_20150129_2041'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='invoice',
+            name='void',
+            field=models.BooleanField(default=False),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='payment',
+            name='method',
+            field=models.CharField(max_length=2, choices=[(b'C', b'Cash'), (b'I', b'Internal'), (b'E', b'External'),
+                                                          (b'SU', b'SU Core')]),
+            preserve_default=True,
+        ),
+    ]
