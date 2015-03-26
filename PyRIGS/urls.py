4002206fa94ecb2fbd9969e19989b1960b929c3b
tomtom5152 <tom@codedinternet.com>
2015-03-26 13:44:50 +0000

    Bug fix for #27

--- PyRIGS/urls.py
+++ PyRIGS/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
+from registration.forms import RegistrationFormUniqueEmail
+from registration.backends.default.views import RegistrationView
 import RIGS
 
 urlpatterns = patterns('',
@@ -10,6 +12,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^', include('RIGS.urls')),
+    url('^user/register/', RegistrationView.as_view(form_class=RegistrationFormUniqueEmail), name="registration_register"),
     url('^user/', include('django.contrib.auth.urls')),
     url('^user/', include('registration.backends.default.urls')),
 
