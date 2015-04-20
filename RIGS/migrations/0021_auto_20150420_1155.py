d60d6b66d0b89a5723bda74cd9c243344052ce67
David Taylor <david@taylorhq.com>
2015-04-20 12:57:42 +0100

    Added api_key database migrations. Also includes other outstanding migrations:     - Change Meta options on invoice     - Add field api_key to profile     - Alter field collector on event     - Alter field initials on profile

---
+++ RIGS/migrations/0021_auto_20150420_1155.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('RIGS', '0020_auto_20150303_0243'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='invoice',
+            options={'ordering': ['-invoice_date'], 'permissions': (('view_invoice', 'Can view Invoices'),)},
+        ),
+        migrations.AddField(
+            model_name='profile',
+            name='api_key',
+            field=models.CharField(max_length=40, null=True, editable=False, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='event',
+            name='collector',
+            field=models.CharField(max_length=255, null=True, verbose_name=b'Collected By', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='profile',
+            name='initials',
+            field=models.CharField(max_length=5, unique=True, null=True),
+            preserve_default=True,
+        ),
+    ]
