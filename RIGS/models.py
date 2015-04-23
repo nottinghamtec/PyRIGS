2b79b17fbf40c24c5d6b66c46067780be613079a
David Taylor <david@taylorhq.com>
2015-04-23 23:58:54 +0100

    Added PO field to event form

--- RIGS/models.py
+++ RIGS/models.py
@@ -255,7 +255,7 @@ class Event(models.Model, RevisionMixin):
     # Monies
     payment_method = models.CharField(max_length=255, blank=True, null=True)
     payment_received = models.CharField(max_length=255, blank=True, null=True)
-    purchase_order = models.CharField(max_length=255, blank=True, null=True)
+    purchase_order = models.CharField(max_length=255, blank=True, null=True, verbose_name='Purchase Order')
     collector = models.CharField(max_length=255, blank=True, null=True, verbose_name='Collected By')
 
     # Calculated values
