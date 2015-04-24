dc425122e688582f67ef3c3ebbc9265e19f6b3ec
Tom Price <tom@codedinternet.com>
2015-04-24 17:06:22 +0100

    Fix typo

--- PyRIGS/wsgi.py
+++ PyRIGS/wsgi.py
@@ -7,7 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 import os
-Add a comment to this line
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PyRIGS.settings")
 
 from django.core.wsgi import get_wsgi_application
