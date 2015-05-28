6754064ebc24e8ead29722f82cfadad1f65f383d
David Taylor <david@taylorhq.com>
2015-05-28 20:00:46 +0100

    Scrap that - bad idea!

--- PyRIGS/settings.py
+++ PyRIGS/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
     'RIGS',
 
     'debug_toolbar',
