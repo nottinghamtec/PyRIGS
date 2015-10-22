32aabe14c77780e0855f5747465605c9a81c0974
Tom Price <tom@codedinternet.com>
2015-10-22 13:39:19 +0100

    Add training models to admin interface

--- training/admin.py
+++ training/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from training import models
 
 # Register your models here.
+admin.site.register(models.TrainingCategory)
+admin.site.register(models.TrainingItem)
+admin.site.register(models.TrainingRecord)
+admin.site.register(models.TrainingLevelRecord)
