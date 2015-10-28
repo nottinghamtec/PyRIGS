9379050e1532f33c3b30991ebc5f0a4289321f98
Tom Price <tom@codedinternet.com>
2015-10-28 17:41:02 +0000

    Add basic detail view for training record.
    Only committing because I have other things to do

--- training/views.py
+++ training/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from training import models
 
 # Create your views here.
+
+
+class TrainingRecordDetailView(generic.DetailView):
+    model = models.TrainingRecord
