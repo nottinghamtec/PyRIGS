0e8a23733fdd3f16cfee653c5cad5b57e8ebc351
Harry Bridge <harrybridge01@gmail.com>
2018-03-06 14:02:24 +0000

    Working CRUD views

--- urls.py
+++ urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from assets import views
 
 urlpatterns = [
-
+    path('', views.Index.as_view(), name='index'),
+    path('assets/', views.AssetList.as_view(), name='asset_list'),
+    path('assets/<int:pk>/', views.AssetDetail.as_view(), name='asset_detail'),
+    path('assets/create', views.AssetCreate.as_view(), name='asset_create'),
+    path('assets/<int:pk>/update', views.AssetUpdate.as_view(), name='asset_update'),
+    path('assets/<int:pk>/delete', views.AssetDelete.as_view(), name='asset_delete'),
 ]
--- views.py
+++ views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
+from assets import models, forms
 
-# Create your views here.
+
+class Index(generic.TemplateView):
+    template_name = 'index.html'
+
+
+class AssetList(generic.ListView):
+    model = models.Asset
+    template_name = 'assets/asset_list.html'
+
+
+class AssetDetail(generic.DetailView):
+    model = models.Asset
+    template_name = 'assets/asset_detail.html'
+
+
+class AssetCreate(generic.CreateView):
+    model = models.Asset
+    fields = '__all__'
+    template_name = 'assets/asset_form.html'
+    # success_url = reverse_lazy('asset_list')
+
+
+class AssetUpdate(generic.UpdateView):
+    model = models.Asset
+    fields = '__all__'
+    template_name = 'assets/asset_form.html'
+
+
+class AssetDelete(generic.DeleteView):
+    model = models.Asset
+    template_name = 'confirm_delete.html'
+    success_url = reverse_lazy('asset_list')
