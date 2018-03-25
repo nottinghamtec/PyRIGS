3933739fc7b866057439963297849cf7d684ea31
Johnathan Graydon <johnathan99j@yahoo.com>
2018-03-25 14:58:14 +0100

    Add 127.0.0.1 to Allowed_Hosts for debug

--- PyRIGS/settings.py
+++ PyRIGS/settings.py
@@ -35,6 +35,7 @@ if STAGING:
 if DEBUG:
     ALLOWED_HOSTS.append('localhost')
     ALLOWED_HOSTS.append('example.com')
+    ALLOWED_HOSTS.append('127.0.0.1')
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 if not DEBUG:
