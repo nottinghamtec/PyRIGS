47c46f4cc40916295dbac84a9cd489153d75ca18
Harry Bridge <harrybridge01@gmail.com>
2018-07-16 00:30:39 +0100

    Updated new template locations

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
-from assets import models, forms
+from assets import models
 
 
 class Index(generic.TemplateView):
@@ -10,25 +10,30 @@ class Index(generic.TemplateView):
 
 class AssetList(generic.ListView):
     model = models.Asset
-    template_name = 'assets/asset_list.html'
+    template_name = 'asset_list.html'
 
 
 class AssetDetail(generic.DetailView):
     model = models.Asset
-    template_name = 'assets/asset_detail.html'
+    template_name = 'asset_detail.html'
 
 
 class AssetCreate(generic.CreateView):
     model = models.Asset
     fields = '__all__'
-    template_name = 'assets/asset_form.html'
+    template_name = 'asset_form.html'
     # success_url = reverse_lazy('asset_list')
 
 
 class AssetUpdate(generic.UpdateView):
     model = models.Asset
     fields = '__all__'
-    template_name = 'assets/asset_form.html'
+    template_name = 'asset_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AssetUpdate, self).get_context_data(**kwargs)
+        context['form_header'] = 'Update'
+        return context
 
 
 class AssetDelete(generic.DeleteView):
