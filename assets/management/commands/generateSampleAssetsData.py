0c12a3efdb04dc91c19e887d80a7dceab1fd01d6
FreneticScribbler <aj@aronajones.com>
2020-02-16 18:46:12 +0000

    Improve sample data generator
    Does reversion properly and sets colours for asset statuses

--- assets/management/commands/generateSampleAssetsData.py
+++ assets/management/commands/generateSampleAssetsData.py
@@ -1,7 +1,9 @@
 import random
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
+from reversion import revisions as reversion
 from assets import models
+from RIGS import models as rigsmodels
 
 
 class Command(BaseCommand):
@@ -29,17 +31,19 @@ class Command(BaseCommand):
             models.AssetCategory.objects.create(name=cat)
 
     def create_statuses(self):
-        statuses = [('In Service', True), ('Lost', False), ('Binned', False), ('Sold', False), ('Broken', False)]
+        statuses = [('In Service', True, 'success'), ('Lost', False, 'warning'), ('Binned', False, 'danger'), ('Sold', False, 'danger'), ('Broken', False, 'warning')]
 
         for stat in statuses:
-            models.AssetStatus.objects.create(name=stat[0], should_show=stat[1])
+            models.AssetStatus.objects.create(name=stat[0], should_show=stat[1], display_class=stat[2])
 
     def create_suppliers(self):
         suppliers = ["Acme, inc.", "Widget Corp", "123 Warehousing", "Demo Company", "Smith and Co.", "Foo Bars", "ABC Telecom", "Fake Brothers", "QWERTY Logistics", "Demo, inc.", "Sample Company", "Sample, inc", "Acme Corp", "Allied Biscuit", "Ankh-Sto Associates", "Extensive Enterprise", "Galaxy Corp", "Globo-Chem", "Mr. Sparkle", "Globex Corporation", "LexCorp", "LuthorCorp", "North Central Positronics", "Omni Consimer Products", "Praxis Corporation", "Sombra Corporation", "Sto Plains Holdings", "Tessier-Ashpool", "Wayne Enterprises", "Wentworth Industries", "ZiffCorp", "Bluth Company", "Strickland Propane", "Thatherton Fuels", "Three Waters", "Water and Power", "Western Gas & Electric", "Mammoth Pictures", "Mooby Corp", "Gringotts", "Thrift Bank", "Flowers By Irene", "The Legitimate Businessmens Club", "Osato Chemicals", "Transworld Consortium", "Universal Export", "United Fried Chicken", "Virtucon", "Kumatsu Motors", "Keedsler Motors", "Powell Motors", "Industrial Automation", "Sirius Cybernetics Corporation", "U.S. Robotics and Mechanical Men", "Colonial Movers", "Corellian Engineering Corporation", "Incom Corporation", "General Products", "Leeding Engines Ltd.", "Blammo",  # noqa
                      "Input, Inc.", "Mainway Toys", "Videlectrix", "Zevo Toys", "Ajax", "Axis Chemical Co.", "Barrytron", "Carrys Candles", "Cogswell Cogs", "Spacely Sprockets", "General Forge and Foundry", "Duff Brewing Company", "Dunder Mifflin", "General Services Corporation", "Monarch Playing Card Co.", "Krustyco", "Initech", "Roboto Industries", "Primatech", "Sonky Rubber Goods", "St. Anky Beer", "Stay Puft Corporation", "Vandelay Industries", "Wernham Hogg", "Gadgetron", "Burleigh and Stronginthearm", "BLAND Corporation", "Nordyne Defense Dynamics", "Petrox Oil Company", "Roxxon", "McMahon and Tate", "Sixty Second Avenue", "Charles Townsend Agency", "Spade and Archer", "Megadodo Publications", "Rouster and Sideways", "C.H. Lavatory and Sons", "Globo Gym American Corp", "The New Firm", "SpringShield", "Compuglobalhypermeganet", "Data Systems", "Gizmonic Institute", "Initrode", "Taggart Transcontinental", "Atlantic Northern", "Niagular", "Plow King", "Big Kahuna Burger", "Big T Burgers and Fries", "Chez Quis", "Chotchkies", "The Frying Dutchman", "Klimpys", "The Krusty Krab", "Monks Diner", "Milliways", "Minuteman Cafe", "Taco Grande", "Tip Top Cafe", "Moes Tavern", "Central Perk", "Chasers"]  # noqa
 
-        for supplier in suppliers:
-            models.Supplier.objects.create(name=supplier)
+        with reversion.create_revision():
+            for supplier in suppliers:
+                reversion.set_user(random.choice(rigsmodels.Profile.objects.all()))
+                models.Supplier.objects.create(name=supplier)
 
     def create_assets(self):
         asset_description = ['Large cable', 'Shiny thing', 'New lights', 'Really expensive microphone', 'Box of fuse flaps', 'Expensive tool we didn\'t agree to buy', 'Cable drums', 'Boring amount of tape', 'Video stuff no one knows how to use', 'More amplifiers', 'Heatshrink']
@@ -48,22 +52,24 @@ class Command(BaseCommand):
         statuses = models.AssetStatus.objects.all()
         suppliers = models.Supplier.objects.all()
 
-        for i in range(100):
-            asset = models.Asset(
-                asset_id='{}'.format(models.Asset.get_available_asset_id()),
-                description=random.choice(asset_description),
-                category=random.choice(categories),
-                status=random.choice(statuses),
-                date_acquired=timezone.now().date()
-            )
+        with reversion.create_revision():
+            for i in range(100):
+                reversion.set_user(random.choice(rigsmodels.Profile.objects.all()))
+                asset = models.Asset(
+                    asset_id='{}'.format(models.Asset.get_available_asset_id()),
+                    description=random.choice(asset_description),
+                    category=random.choice(categories),
+                    status=random.choice(statuses),
+                    date_acquired=timezone.now().date()
+                )
 
-            if i % 4 == 0:
-                asset.parent = models.Asset.objects.order_by('?').first()
+                if i % 4 == 0:
+                    asset.parent = models.Asset.objects.order_by('?').first()
 
-            if i % 3 == 0:
-                asset.purchased_from = random.choice(suppliers)
-            asset.clean()
-            asset.save()
+                if i % 3 == 0:
+                    asset.purchased_from = random.choice(suppliers)
+                asset.clean()
+                asset.save()
 
     def create_cables(self):
         asset_description = ['The worm', 'Harting without a cap', 'Heavy cable', 'Extension lead', 'IEC cable that we should remember to prep']
