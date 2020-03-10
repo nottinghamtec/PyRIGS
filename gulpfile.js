3b8789e49e2e5d2708e1833370f5eb3798d46d43
FreneticScribbler <aj@aronajones.com>
2020-03-10 16:46:53 +0000

    More refactoring of rigboard

--- gulpfile.js
+++ gulpfile.js
@@ -7,20 +7,26 @@ var rename = require('gulp-rename');
 var terser = require('gulp-terser');
 var sass = require('gulp-sass');
 var flatten = require('gulp-flatten');
-let cleanCSS = require('gulp-clean-css');
+var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('autoprefixer')
+var postcss = require('gulp-postcss')
+var sourcemaps = require('gulp-sourcemaps');
 
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
   return gulp.src('web_assets/scss/**/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(flatten())
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('RIGS/static/css'));
 });
 
 gulp.task('scripts', function() {
-    return gulp.src(['web_assets/js/**/*.js', 'node_modules/bootstrap/**/bootstrap.min.js', 'node_modules/popper\.js/**/popper.js', 'node_modules/moment/**/moment.js', 'node_modules/@fortawesome/fontawesome-free/js/all.js'])
+    return gulp.src(['web_assets/js/**/*.js', 'node_modules/bootstrap/**/bootstrap.min.js', 'node_modules/popper\.js/**/popper.js', 'node_modules/moment/moment.js', 'node_modules/@fortawesome/fontawesome-free/js/all.js', 'node_modules/jquery/dist/jquery.js'])
     .pipe(flatten())
     .pipe(terser())
     .pipe(gulp.dest('RIGS/static/js'));
