b8ea3d3d425c5e34eaefaa273b036ac385ef6bb9
FreneticScribbler <aj@aronajones.com>
2020-03-15 10:10:33 +0000

    Dependency update

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,11 @@ var { exec } = require("child_process");
 sass.compiler = require('node-sass');
 
 function sass() {
-  return gulp.src(["RIGS/scss/**/*.scss", "node_modules/fullcalendar/dist/fullcalendar.css", 'node_modules/ajax-bootstrap-select/dist/css/ajax-bootstrap-select.min.css', 'node_modules/autocompleter/autocompleter.min.css', 'node_modules/@activix/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css'])
+  return gulp.src(["RIGS/scss/**/*.scss",
+                    "node_modules/fullcalendar/dist/fullcalendar.css",
+                    'node_modules/ajax-bootstrap-select/dist/css/ajax-bootstrap-select.min.css',
+                    'node_modules/autocompleter/autocompleter.min.css',
+                    'node_modules/@activix/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css'])
     .pipe(sourcemaps.init())
     .pipe(flatten())
     .pipe(sass().on('error', sass.logError))
@@ -29,8 +33,21 @@ function sass() {
 }
 
 function scripts() {
-    return gulp.src([//'RIGS/static/js/**/.js',
- 'node_modules/bootstrap/**/bootstrap.min.js', 'node_modules/popper.js/**/popper.js', 'node_modules/moment/min/moment.min.js', 'node_modules/@fortawesome/fontawesome-free/js/all.js', 'node_modules/jquery/dist/jquery.js', 'node_modules/fullcalendar/dist/fullcalendar.js', 'node_modules/ajax-bootstrap-select/dist/js/ajax-bootstrap-select.min.js', 'node_modules/konami.js', 'node_modules/autocompleter/autocompleter.min.js', 'node_modules/@activix/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js'])
+    return gulp.src(['RIGS/static/js/src/**/.js',
+                    'node_modules/jquery/dist/jquery.js',
+                    'node_modules/ravenjs/ravenjs.js', //TODO Upgrade to Sentry
+                    /* Bootstrap Plugins */
+                    'node_modules/bootstrap/js/dist/dropdown.js',
+                    'node_modules/bootstrap/js/dist/collapse.js',
+                    'node_modules/bootstrap/js/dist/modal.js',
+                    'node_modules/@fortawesome/fontawesome-free/js/all.js',
+                    'node_modules/popper.js/**/popper.js',
+                    'node_modules/moment/min/moment.min.js',
+                    'node_modules/fullcalendar/dist/fullcalendar.js',
+                    'node_modules/ajax-bootstrap-select/dist/js/ajax-bootstrap-select.min.js',
+                    'node_modules/konami/konami.js',
+                    'node_modules/autocompleter/autocompleter.min.js',
+                    'node_modules/@activix/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js'])
     .pipe(flatten())
     .pipe(terser())
     .pipe(gulp.dest('RIGS/static/js'))
