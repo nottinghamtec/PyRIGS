36556dea332225316f175d2387a10ab789295344
FreneticScribbler <aj@aronajones.com>
2020-03-17 20:07:14 +0000

    Add the usual set of fields to supplier and update form accordingly

--- assets/models.py
+++ assets/models.py
@@ -45,6 +45,11 @@ class Supplier(models.Model, RevisionMixin):
         ordering = ['name']
 
     name = models.CharField(max_length=80)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
+    notes = models.TextField(blank=True, null=True)
 
     def get_absolute_url(self):
         return reverse('supplier_list')
