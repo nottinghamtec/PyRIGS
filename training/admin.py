3258094a81ce31816fd177daa2ca9fb83a884879
FreneticScribbler <aj@aronajones.com>
2022-01-08 17:51:11 +0000

    Admin register a bunch more stuff

--- training/admin.py
+++ training/admin.py
@@ -6,3 +6,6 @@ from reversion.admin import VersionAdmin
 admin.site.register(models.TrainingCategory, VersionAdmin)
 admin.site.register(models.TrainingItem, VersionAdmin)
 admin.site.register(models.TrainingLevel, VersionAdmin)
+admin.site.register(models.TrainingItemQualification, VersionAdmin)
+admin.site.register(models.TrainingLevelQualification, VersionAdmin)
+admin.site.register(models.TrainingLevelRequirement, VersionAdmin)
