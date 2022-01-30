34c03e379d2b2994cc32a604ae05681ecc21e09e
FreneticScribbler <aj@aronajones.com>
2022-01-30 11:23:54 +0000

    Switch to using competency assessed for leaderboard

--- training/views.py
+++ training/views.py
@@ -111,7 +111,7 @@ class TraineeList(generic.ListView):
         if self.request.GET.get('is_supervisor', ''):
             filt = filt & Q(is_supervisor=True)
 
-        return self.model.objects.filter(filt).annotate(num_qualifications=Count('qualifications_obtained')).order_by('-num_qualifications').prefetch_related('level_qualifications', 'qualifications_obtained', 'qualifications_obtained__item')
+        return self.model.objects.filter(filt).annotate(num_qualifications=Count('qualifications_obtained', filter=Q(qualifications_obtained__depth=models.TrainingItemQualification.PASSED_OUT))).order_by('-num_qualifications').prefetch_related('level_qualifications', 'qualifications_obtained', 'qualifications_obtained__item')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
